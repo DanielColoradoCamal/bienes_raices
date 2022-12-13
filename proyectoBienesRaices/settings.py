c5023ba212b80c554eb5ce4bb10d0637770bd884
DanielColoradoCamal <daniel.colorado.dev@gmail.com>
2022-12-13 00:18:50 -0600

    Agregando app blog y creando forms

--- proyectoBienesRaices/settings.py
+++ proyectoBienesRaices/settings.py
@@ -1,4 +1,9 @@
+import os
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,10 +13,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)l0fo2jls4!nqw(z0w=*%d==*yw#_i-7@vi!yxluy3a#qyybfa'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -25,6 +30,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'proyectoBienesRaices',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -42,7 +50,7 @@ ROOT_URLCONF = 'proyectoBienesRaices.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
